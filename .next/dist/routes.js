f87a22b1d9b833d9acf5a2a652bb812b071907e6
Iftifar Taz <iftifar.taz@northsouth.edu>
2019-09-25 20:53:41 +0600

    delete .next files

--- .next/dist/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/', '/').add('/campaigns/new', '/campaigns/new').add('/campaigns/:address', '/campaigns/show').add('/campaigns/:address/requests', '/campaigns/requests/index').add('/campaigns/:address/requests/new', '/campaigns/requests/new');
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNLLEFBREwsSUFDUyxBQURULEtBQ2MsQUFEZCxLQUVLLEFBRkwsSUFFUyxBQUZULGtCQUUyQixBQUYzQixrQkFHSyxBQUhMLElBR1MsQUFIVCx1QkFHZ0MsQUFIaEMsbUJBSUssQUFKTCxJQUlTLEFBSlQsZ0NBSXlDLEFBSnpDLDZCQUtLLEFBTEwsSUFLUyxBQUxULG9DQUs2QyxBQUw3Qzs7QUFPQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6IkQ6L0NvZGVzL0VUSC9kQXBwL2NhbXBhaWduIn0=
